c2b2af80b2e12606e87b1a72bdbadf1738e4f3ce
jkwilches <jkwilches.web@gmail.com>
2025-01-08 11:38:46 -0500

    Creación de carpetas de cursos

---
+++ Fundamentos-JavaScript/clase-variables.js
@@ -0,0 +1,12 @@
+let cajaDeAndy = `Woody`;
+console.log(cajaDeAndy);
+
+// Lo No permitido
+let c = `woody`;
+let cda = `woody`;
+let pcAndy = `Woody`;
+
+// Lo SI permitido
+let primerTrasteoDeAndy = `Woody`;
+let urlDelUsuario = `https://www.google.com`;
+let idDelUsuario = `12343456`;
---
+++ Python/Curso-Python/clase3.py
@@ -0,0 +1,3 @@
+saludo = "Hola"
+nombre = "Juan" 
+print (saludo,nombre)
